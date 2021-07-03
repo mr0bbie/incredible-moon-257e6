44103d073bea26e1f8574c610d9305eff3921ac2
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2021-07-03 17:15:01 +0700

    fix undefined error payment server

--- functions/payment.js
+++ functions/payment.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
     additional_notes
   } = order;
 
-  const messageLetters = toSubmit.message?.replace(/ /g,'');
+  const messageLetters = message?.replace(/ /g,'');
   const letters = messageLetters ? messageLetters.length : 0;
   const subtotal = letters <= 3 ? 450 : letters * 150;
   const totalPayment = Math.round(subtotal * 1.1);
